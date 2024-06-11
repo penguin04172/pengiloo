737bbf7336face66644525f7375e523b76f2a51a
penguin04172 <sylin4172@gmail.com>
2024-06-11 16:34:39 +0800

    Update main.py: add router api, web

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import fastapi
 from models.database import db
 
+from api import team
+from web import index
+
 app = fastapi.FastAPI()
+app.include_router(team.router)
+app.include_router(index.router)
+
 db.bind(provider='sqlite', filename='db.sqlite3', create_db=True)
 db.generate_mapping(create_tables=True)
