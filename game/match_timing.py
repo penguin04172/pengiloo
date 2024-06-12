936d3058165b9306daf9bce7b938fca0116f5270
penguin04172 <sylin4172@gmail.com>
2024-06-12 14:03:31 +0800

    Add game: match_timing

---
+++ game/match_timing.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from pydantic import BaseModel
+
+class MatchTiming(BaseModel):
+    warmup_duration_sec: int = 0
+    auto_duration_sec: int = 15
+    pause_duration_sec: int = 3
+    teleop_duration_sec: int = 135
+    warning_remaining_duration_sec: int = 20
+    timeout_duration_sec: int = 0
+
+match_timing = MatchTiming()
+
+def get_duration_to_auto_end():
+    return match_timing.warmup_duration_sec + match_timing.auto_duration_sec
+
+def get_duration_to_teleop_start():
+    return match_timing.warmup_duration_sec + match_timing.auto_duration_sec + match_timing.pause_duration_sec
+
+def get_duration_to_teleop_end():
+    return match_timing.warmup_duration_sec + match_timing.auto_duration_sec + match_timing.pause_duration_sec + match_timing.teleop_duration_sec
