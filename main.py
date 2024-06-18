c10384949491078bdcdd27de4bc13d221df79138
penguin04172 <sylin4172@gmail.com>
2024-06-18 16:44:39 +0800

    Change DB to memory

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import fastapi
 from models.database import db
 
-from api import team
+from api import match, team, event
 from web import index
 
 app = fastapi.FastAPI()
+
 app.include_router(team.router)
+app.include_router(event.router)
+app.include_router(match.router)
+
 app.include_router(index.router)
 
-db.bind(provider='sqlite', filename='db.sqlite3', create_db=True)
+# db.bind(provider='sqlite', filename='db.sqlite3', create_db=True)
+db.bind(provider='sqlite', filename=':memory:', create_db=True)
 db.generate_mapping(create_tables=True)
