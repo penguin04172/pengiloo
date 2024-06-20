8ad3a40aac38b6a043ace402822e17b06239bb09
penguin04172 <sylin4172@gmail.com>
2024-06-20 16:13:58 +0800

    Add game.rule with unittest

---
+++ game/rule_test.py
@@ -0,0 +1,15 @@
+import unittest
+from .rule import *
+
+class RuleTest(unittest.TestCase):
+    def test_get_rule_by_id(self):
+        self.assertIsNone(get_rule_by_id(0))
+        self.assertEqual(rules[0], list(get_rule_by_id(1).model_dump().values()))
+        self.assertEqual(rules[20], list(get_rule_by_id(21).model_dump().values()))
+        self.assertIsNone(get_rule_by_id(1000))
+
+    def test_get_all_rules(self):
+        all_rules = get_all_rules()
+        self.assertEqual(len(rules), len(all_rules))
+        for rule in rules:
+            self.assertEqual(rule, list(all_rules[rule[0]].model_dump().values()))
