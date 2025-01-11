b462d1977755b88c2c5fc955046190b02b0dcfe2
penguin04172 <sylin4172@gmail.com>
2025-01-11 22:23:34 +0800

    Update websocket.notifier with docstring

--- websocket/notifier.py
+++ websocket/notifier.py
@@ -8,16 +8,26 @@ NOTIFY_QUEUE_SIZE = 5
 
 
 class Notifier:
+    """Notifier class."""
+
     listeners: set[WebSocket]
     message_type: str
     message_producer: Callable[..., Any] | None
 
     def __init__(self, message_type: str, message_producer: Callable[..., Any] | None = None):
+        """Create a new Notifier instance.
+
+        Args:
+            message_type (str): _description_
+            message_producer (Callable[..., Any] | None, optional): _description_. Defaults to None.
+        """
         self.listeners = set()
         self.message_type = message_type
         self.message_producer = message_producer
 
     class MessageEnvelope:
+        """Message envelope class."""
+
         type: str
         payload: Any
 
@@ -26,28 +36,45 @@ class Notifier:
             self.payload = payload
 
     async def connect(self, websocket: WebSocket):
+        """Connect a new listener.
+
+        Args:
+            websocket (WebSocket): _description_
+        """
         await websocket.accept()
         self.listeners.add(websocket)
 
     def disconnect(self, websocket: WebSocket):
+        """Disconnect a listener.
+
+        Args:
+            websocket (WebSocket): _description_
+        """
         self.listeners.remove(websocket)
 
     async def notify(self):
+        """Notify all listeners with a message."""
         message_body = self.message_producer() if self.message_producer else None
         await self.notify_with_message(message_body)
 
     async def notify_with_message(self, message: Any):
+        """Notify all listeners with a message.
+
+        Args:
+            message (Any): _description_
+        """
         message = self.MessageEnvelope(type=self.message_type, payload=message)
         for listener in self.listeners:
             await self.notify_listener(listener, message)
 
     async def notify_listener(self, listener: WebSocket, message: MessageEnvelope):
-        """
-        非阻塞地通知單個監聽者。
-        捕捉錯誤（例如連接中斷），並在失敗時從監聽列表中移除該 WebSocket。
+        """Notify a single listener with a message.
+
+        Args:
+            listener (WebSocket): _description_
+            message (MessageEnvelope): _description_
         """
         try:
-            # 非阻塞地發送訊息
             await listener.send_json(vars(message))
         except Exception as e:
             logging.warning(f'Failed to send message to listener: {e}')
